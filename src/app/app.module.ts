6d4caee643f717b0f51a09b2a53eeef689034d46
Abdulaziz Ladan <abdulazizladan@gmail.com>
2021-05-05 10:42:44 +0100

    task: Create page under construction page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -11,6 +15,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatCardModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
