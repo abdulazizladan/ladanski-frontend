a2cb55dfcc4e3dab88b7ed06b1e49392fde96bda
Abdulaziz Ladan <abdulazizladan@gmail.com>
2021-06-23 21:07:53 +0100

    task: Update angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { FormsModule } from '@angular/forms';
-//import { ChartsModule } from 'ng2-charts';
-
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ProductComponent } from './components/product/product.component';
-import { ProducDetailsComponent } from './components/produc-details/produc-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductComponent,
-    ProducDetailsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    //ChartsModule,
-    FlexLayoutModule,    
-    MatIconModule,
-    MatToolbarModule,
-    MatCardModule,
-    MatButtonModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
