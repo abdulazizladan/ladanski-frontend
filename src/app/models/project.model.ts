9fa08abfc8b145d1e7ccd03cd2225565d3a9c7e0
Abdulaziz Ladan <abdulazizaldan@gmail.com>
2022-02-17 21:57:54 +0100

    task: Add hyperlink to biznet web app

--- src/app/models/project.model.ts
+++ src/app/models/project.model.ts
@@ -1,5 +1,7 @@
 export interface Project{
   title: string,
   description: string,
-  image: string
+  image: string,
+  disabled: boolean,
+  url: string
 }
--- src/app/services/in-memory-db-service.service.ts
+++ src/app/services/in-memory-db-service.service.ts
@@ -9,10 +9,10 @@ export class InMemoryDbServiceService implements InMemoryDbService {
 
   createDb() {
     let projects: Project[] = [
-      { title: "BizNet", description: "A business-oriented social network.", image: "" },
-      { title: "MediCare", description: "A medical records management system.", image: "" },
-      { title: "ProMon", description: "A project records management system.", image: "" },
-      { title: "ItSchool", description: "A learning management system.", image: "" }
+      { title: "BizNet", description: "A business-oriented social network.", image: "", disabled: false, url: 'https://www.biznet.com.ng' },
+      { title: "MediCare", description: "A medical records management system.", image: "", disabled: true, url: ''},
+      { title: "ProMon", description: "A project records management system.", image: "", disabled: true, url: '' },
+      { title: "ItSchool", description: "A learning management system.", image: "", disabled: true, url: '' }
     ];
     return {projects};
   }
