6be9288544ff3ae270997c88c25ed7a81f1288e9
Abdulaziz Ladan <abdulazizaldan@gmail.com>
2022-04-03 12:32:37 +0100

    task: Add sections to home page

--- src/app/services/in-memory-db-service.service.ts
+++ src/app/services/in-memory-db-service.service.ts
@@ -10,7 +10,7 @@ export class InMemoryDbServiceService implements InMemoryDbService {
   createDb() {
     let projects: Project[] = [
       { title: "BizNet", description: "A business-oriented social network.", logo: "/assets/logos/biznet.png", image: "", disabled: false, url: 'https://www.biznet.com.ng' },
-      { title: "MediCare", description: "A medical records management system.", logo: "", image: "", disabled: true, url: ''},
+      { title: "MediCare", description: "A medical records management system.", logo: "/assets/logos/medicare.png", image: "", disabled: true, url: ''},
       { title: "ProMon", description: "A project records management system.", logo: "", image: "", disabled: true, url: '' },
       { title: "ItSchool", description: "A learning management system.", logo: "", image: "", disabled: true, url: '' }
     ];
